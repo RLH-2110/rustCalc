fe2f618ffab66ae5ecb47b691909dacce24362cf
RLH-2110 <Roland.hartung@gmx.net>
2025-03-27 13:27:53 +0100

    refactored old code and added the skeleton for converting the args to tokens

--- main.rs
+++ main.rs
@@ -1,23 +1,44 @@
 use std::env;
 
+#[derive(PartialEq, Eq, Debug)]
+enum TokenType{
+	Number,
+	Operation,
+}
+
+struct Token {
+	id: TokenType,
+	value: u32,
+}
+
 fn main(){
-	let orig_argv: Vec<String> = env::args().collect();
-    let mut argc = orig_argv.len();
+	let argv: Vec<String> = env::args().collect();
+	let mut args: String = "".to_string();
 
-	if argc == 1{
-		println!("usage: {} [expression]",orig_argv[0]);
+	if argv.len() == 1{
+		println!("usage: {} [expression]",argv[0]);
+	}
+	
+	for arg in &argv[1..]{
+		println!("{}",arg);
+		args.push_str(arg);
 	}
 
-	let argv = &orig_argv[1..]; argc-=1;
+	println!("{}",args);
 
-	let mut i = 0;
-	loop{
-		if i >= argc{
-			break;
+	let mut expression: Vec<Token> = Vec::new();
+	expression.push(Token {id: TokenType::Number, value: 15});
+
+	if expression[0].id == TokenType::Number{
+		println!("Number {}",expression[0].value);
+	}
+	else {
+		if expression[0].id == TokenType::Operation {
+			println!("Operation {}",expression[0].value);
+		}else{
+			unimplemented!();
 		}
-		println!("{}",argv[i]); i+=1;
 	}
-
 }	
 
 	
