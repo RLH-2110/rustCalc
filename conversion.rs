41182567ab7ee01ae59d1278bcff844f9b650945
RLH-2110 <roland.hartung@gmx.net>
2025-04-10 15:31:54 +0200

    added Broken floating point
    tests now work with the debug version for debug asserts
    added broken floating point. it works fine, expect for mul and div
    added some tests

---
+++ conversion.rs
@@ -0,0 +1,127 @@
+
+use tokenize::Operation;
+
+
+// converts number into Operation
+
+pub fn op_from_num(num: i64) -> Result<Operation,i32> {
+	if num > Operation::Div as i64 || num < 0{
+		return Err(crate::EXIT_INVAL_OPERATION_ID);
+	}
+
+	unsafe { return Ok(std::mem::transmute(num)); }
+}
+
+
+pub fn string_to_fp(str: &String) -> Result<i64,i32>{
+
+	let fp: u8;
+	unsafe { fp = crate::FIXED_POINT; }
+
+	let snum: Vec<String> = str.splitn(2,".").map(|s| s.to_string()).collect(); // thanks deepseek for figuring out the mapping bullshit.
+	let mut upper: i64 = 0;
+	let lower: i64;
+
+	if snum[0] != "" {
+		upper = match snum[0].parse::<u32>(){
+        Ok(val) => val,
+        Err(_) => { println!("Numbers must be nummbers between 0 and 4294967295!"); return Err(crate::EXIT_INPUT_OVERFLOW);},
+      } as i64;
+	}
+
+	upper = checked_shl(upper,fp)?;
+
+	if snum.len() == 1 || snum[1] == "" {
+		return Ok(upper);
+	}
+
+	lower = string_make_fp(fp,&snum[1])?;
+
+	return Ok(upper|lower);
+} 
+
+// helper for function above
+fn string_make_fp(fp: u8, str: &String) -> Result<i64,i32> {
+	debug_assert!(fp != 0);
+
+
+	let div_val: i64 = 10i64.pow(str.len() as u32);
+	let mut result: i64 = 0;
+
+	let mut input: i64 = match str.parse::<u32>(){
+        Ok(v) => v,
+        Err(_) => { println!("Numbers must be nummbers between 0 and 4294967295!"); return Err(crate::EXIT_INPUT_OVERFLOW);},
+      } as i64; 
+
+
+    let mut i = fp;
+	loop {
+
+		input*=2;
+		if (input/div_val) > 0 {
+			result+=1;
+			input -= div_val;
+		}
+
+
+		i -= 1;
+		if i == 0{
+			break;
+		}
+		result = result << 1;
+	}
+
+	return Ok(result);
+}
+
+pub fn fp_to_string(num: i64) -> String{
+
+	let fp: u8;
+	unsafe { fp = crate::FIXED_POINT; }
+	let mut string: String = (num >> fp).to_string();
+
+	if fp == 0{
+		return string;
+	}
+
+	string.push_str(".");
+
+	let mut mask: i64 = 0;
+	for _ in 0..fp {
+		mask = mask << 1;
+		mask += 1;
+	}
+
+	let divider:i64 = 1 << fp;
+	let mut n: i64 =  num & mask;
+
+	for _ in 0..fp {
+		n *= 10;
+		let digit: i64 = n / divider;
+		string.push_str(&digit.to_string());
+		n %= divider;
+
+		if n == 0 {
+			break;
+		}
+	}
+
+	return string;
+
+} 
+
+
+// VAL MUST BE A u32 STORED INSIDE A i64!
+// shifts the number left by amount
+//
+// val: u32 value stored as i64 that we want to shift
+// amount: by how much we want to shfit left
+//
+// returns: result of either the i64 result, or EXIT_INPUT_OVERFLOW as i32 exit code
+fn checked_shl(val: i64, amount: u8) -> Result<i64,i32>{
+	let ret = val << amount;
+	if val != ret >> amount{
+		return Err(crate::EXIT_INPUT_OVERFLOW);
+	}
+	return Ok(ret);
+}
--- main.rs
+++ main.rs
@@ -3,6 +3,7 @@ use std::env;
 mod tokenize;
 mod solver;
 mod functions;
+mod conversion;
 
 /*
   return codes:
@@ -35,17 +36,57 @@ pub const EXIT_TRAILING_OP: i32 = 9;
 pub const EXIT_DIV_BY_ZERO: i32 = 10;
 pub const EXIT_INFINITE_LOOP: i32 = 11;
 pub const EXIT_INVAL_UNARY: i32 = 12;
+pub const EXIT_INVAL_OPERATION_ID: i32 = 13;
+pub const EXIT_INPUT_OVERFLOW: i32 = 14;
+pub const EXIT_HUMONGOUS_STRING: i32 = 15;
+//pub const EXIT_NO_ZERO_FOR_FIXED_POINT_MATH: i32 = 16;
 
 pub const EXIT_USAGE: i32 = 100;
+
+static mut FIXED_POINT: u8 = 0; /* how many bits of fixed point */
 
 fn main(){
   let argv: Vec<String> = env::args().collect();
   if argv.len() == 1{
-    println!("usage: {} [expression]",argv[0]);
-    std::process::exit(EXIT_USAGE);
+    print_usage(&argv[0]);
   }
 
-  let args: String = argv[1..].join("");
+  let mut expression_index = 1;
+
+
+  // check for fp flag
+  if argv[1].chars().next().unwrap() == '-'{
+
+    if argv[1].to_uppercase() == "-FP" && argv.len() > 3 { // 3 because [progamm name] [#fp] [number] [expression]
+
+      let val = match argv[2].parse::<u8>(){
+        Ok(val) => val,
+        Err(_) => {print_fp_error();0},
+      };
+
+      if val > 18 {print_fp_error();} // limited to 18, so that the algorim to turn fixed point back to decimal does not overflow
+
+      unsafe { FIXED_POINT = val; }
+      expression_index+=2;
+    }
+
+    if argv[1].to_uppercase() == "-H" || argv[1].to_uppercase() == "-HELP" {
+      print_usage(&argv[0]);
+    }
+
+    }
+
+   
+
+
+
+  let args: String = argv[expression_index..].join("");
+
+  if args.len() > u32::MAX as usize{
+    println!("INPUT TOO BIG, what the fuck are you doing?");
+    std::process::exit(EXIT_HUMONGOUS_STRING);
+  }
+
   let expression = match tokenize::parse(args){
     Ok(v) => v,
     Err(e) => {std::process::exit(e)},
@@ -55,6 +96,21 @@ fn main(){
     Err(e) => {std::process::exit(e);},
   };
 
-  println!("{result}");
+
+  println!("{}", conversion::fp_to_string(result));
 }  
 
+// prints usage and exists
+fn print_usage(progammname: &String){
+  println!("usage: {} (arguments) [expression]\
+    arguments:\n\
+    \t -fp [number]\t - sets the amount of bits used for floating point (defaults to 0)\n\
+    \t -help       \t - prints this menu"
+    ,progammname);
+  std::process::exit(EXIT_USAGE);
+}
+
+fn print_fp_error(){
+   println!("Fixed point value must be between 0 and 18");
+   std::process::exit(EXIT_USAGE);
+}
